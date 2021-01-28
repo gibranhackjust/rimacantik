1d7085e1d36278793d37e16b63b51d5163c4a839
gibranhackjust <78136331+gibranhackjust@users.noreply.github.com>
2021-01-28 16:29:55 +0800

    Add files via upload

---
+++ cyber.sh
@@ -0,0 +1,71 @@
+clear
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet gibran | lolcat
+
+echo $green"No Wa : 085238506497"
+echo $green"Email : gibranfancy@gmail.com"
+echo $green"admin : gibran"
+echo $green"autor : M SYAESAR GIBRAN"
+echo
+echo $cyan"PILIH MENU TOOL : "
+echo
+echo $red"============================"
+echo $purple"[01].Hack Facebook Crack"
+echo $red"============================"
+echo $purple"[02].Hack Instagram(ROOT)"
+echo $red"============================"
+echo $purple"[03].Hack Akun Game online"
+echo $red"============================"
+echo $purple"[04].Hack Diamond GAME"
+echo $red"============================"
+echo $purple"[05].Hack NASA (ROOT)"
+echo $red"============================"
+echo $purple"[06].Root HandPhone"
+echo $red"============================"
+echo $purple"[07].Hack WEBSITE"
+echo $red"============================"
+echo $purple"[08].Hack Satelit"
+echo $red"============================"
+echo $purple"[09].Hack CCTV"
+echo $red"============================"
+echo $purple"[10].Hack Listrik Rumah"
+echo $red"============================"
+echo $purple"[11].Hack Kuota"
+echo $red"============================"
+echo $purple"[12].Hack Pulsa"
+echo $red"============================"
+echo $purple"[13].Hack TV rumah"
+echo $red"============================"
+echo $purple"[14].Hack Running Text"
+echo $red"============================"
+echo $purple"[15].Hack WIFI tetangga"
+echo $red"============================"
+echo $purple"[16].Hack Komputer"
+echo $red"============================"
+echo $purple"[17].Spam SMS"
+echo $red"============================"
+echo $purple"[18].Kirim Virus HP"
+echo $red"============================"
+echo $purple"[19].Hack Tiket Pesawat"
+echo $red"============================"
+echo $purple"[20].Keluar Tools"
+echo $red"============================"
+
